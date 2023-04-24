8fb3e6c828b4393fb61ff27bda15e6c7b5e24eab
maryjoseph11 <georgerhea06@gmail.com>
2023-04-23 21:19:46 -0400

    change of bcg image

--- src/components/ProfileComponent/index.js
+++ src/components/ProfileComponent/index.js
@@ -1,6 +1,6 @@
 import React, {useState} from "react";
 import {useDispatch, useSelector} from "react-redux";
-import "./index.css"
+import "./index.css";
 import {profileThunk} from "../../services/user-thunk";
 import {useNavigate} from "react-router";
 import {Link} from "react-router-dom";
